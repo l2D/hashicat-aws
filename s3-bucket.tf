19292e98d8e432dce84171ebd40c224b0509b55e
l2D <rawipas.chisamut@gmail.com>
2022-07-20 16:54:23 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
-module "s3-bucket" {
+module "s3_bucket" {
   source  = "app.terraform.io/l2D-demo/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "${var.bucket_prefix}"
+  bucket_prefix = "l2d"
 }
